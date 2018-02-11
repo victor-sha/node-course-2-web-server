900e03476f0e78ee587693cd2542f794b5dd541d
Victor Sha <kassini_90@mail.ru>
2018-02-11 17:44:00 +0300

    added new projects page

--- server.js
+++ server.js
@@ -54,6 +54,11 @@ app.get('/bad',(req, res) => {
     });
 })
 
+app.get('/projects',(req, res) => {
+    res.render('projects.hbs',{
+        pageTitle: 'Portfolio Page'
+    }) 
+});
 
 app.listen(port, () => {
     console.log(`Server is up on ${port} port`);
